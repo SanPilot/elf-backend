66fa22174ebf2dce93097048ffebde8e13f271f2
Hussain Khalil <hussainpilot05@gmail.com>
2016-04-02 16:30:12 -0400

    Updated meta submodule

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ Index.js - Runs all modules and starts backend
 
 "use strict";
 
-var config = require("custom_modules/config/index.config.json");
+// Include required files
+var config = require("./custom_modules/config/index.config.json"),
+logger = require("./custom_modules/logger.daemon.js"),
+express = require("express");
+
+// Log information
+logger.log("Welcome to Elf. Index script started up successfully.", 3, false, config.moduleName);
+
+// Start HTTP server
+logger.log("Starting Express HTTP server...", 4, false, config.moduleName);
