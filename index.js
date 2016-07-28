b06e560e19063308e8a218444f7dc44fca1303e0
Hussain Khalil <hussainpilot05@gmail.com>
2016-07-28 01:44:47 -0400

    Fixes to index

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ logger.log("Welcome to Elf. Index script started up successfully.", 3, false, co
 for (var key in config.requireModules) {
   if (config.requireModules.hasOwnProperty(key)) {
     apis[key] = require("./custom_modules/" + config.requireModules[key]);
-    logger.log("Required API module " + key + " with path \"" + config.requireModules[key] + "\".", 6, false, config.moduleName);
+    logger.log("Required API module '" + key + "' with path \"" + config.requireModules[key] + "\".", 6, false, config.moduleName);
   }
 }
 
@@ -37,7 +37,7 @@ logger.log("Starting API server...", 4, false, config.moduleName);
 // Start HTTP server
 var server = http.createServer(()=>{});
 server.listen(config.usePort, () => {
-  logger.log("HTTP server started. Upgrading to WebSocket connection...", 4, false, config.moduleName);
+  logger.log("API server started listening on port " + config.usePort + ".", 4, false, config.moduleName);
 });
 
 // Start WebSocket server
@@ -46,51 +46,62 @@ var wsServer = new webSocketServer({
 });
 
 wsServer.on('request', (request) => {
-  logger.log("Recieved API connection from origin " + request.origin + ".", 6, false, config.moduleName);
-  var connection = request.accept(null, request.origin);
+  setTimeout(() => {
+    logger.log("Recieved API connection from origin " + request.origin + ".", 6, false, config.moduleName);
+    var connection = request.accept(null, request.origin);
+    // limit messages per minute
+    var messagesInLastSecond = 0,
+    freqBlock = false;
 
-  // limit messages per minute
-  var messagesInLastSecond = 0,
-  freqBlock = false;
+    // Reset every second
+    setInterval(() => {
+      messagesInLastSecond = 0;
+    }, 1000);
 
-  // Reset every second
-  setInterval(() => {
-    messagesInLastSecond = 0;
-  }, 1000);
-
-  // accept message
-  connection.on('message', (message) => {
-    if(++messagesInLastSecond > config.freqBlock.messagesAllowedPerSecond) {
-      freqBlock = true;
-      setTimeout(() => {
-        freqBlock = false;
-      }, config.freqBlock.blockTime);
-      logger.log("Possibly malacious requests blocked for being too frequent from " + connection.remoteAddress + ".", 4, true, config.moduleName);
-    }
-    if(!freqBlock) {
-      if (message.type === 'utf8') {
-        var msgObject;
-        try {
-          msgObject = JSON.parse(message.utf8Data);
-        } catch(e) {
-          connection.sendUTF(apiResponses.strings.errors.malformedRequest);
-          return;
-        }
-        if(msgObject.action) {
-          if(config.apiRoutes[msgObject.action]) {
-            apis[config.apiRoutes[msgObject.action][0]][config.apiRoutes[msgObject.action][1]](msgObject, connection);
-          } else {
-            connection.sendUTF(apiResponses.strings.errors.invalidAction);
+    // accept message
+    connection.on('message', (message) => {
+      if(++messagesInLastSecond > config.freqBlock.messagesAllowedPerSecond) {
+        freqBlock = true;
+        setTimeout(() => {
+          freqBlock = false;
+        }, config.freqBlock.blockTime);
+        logger.log("Possibly malacious requests blocked for being too frequent from " + connection.remoteAddress + ".", 4, true, config.moduleName);
+      }
+      if(!freqBlock) {
+        if (message.type === 'utf8') {
+          var msgObject;
+          try {
+            msgObject = JSON.parse(message.utf8Data);
+          } catch(e) {
+            connection.send(apiResponses.strings.errors.malformedRequest);
             return;
           }
+          if(msgObject.id) {
+            if(msgObject.action) {
+              if(config.apiRoutes[msgObject.action]) {
+                apis[config.apiRoutes[msgObject.action][0]][config.apiRoutes[msgObject.action][1]](msgObject, connection);
+              } else {
+                connection.send(apiResponses.concatObj(apiResponses.JSON.errors.invalidAction, {"id": msgObject.id}, true));
+                return;
+              }
+            } else {
+              connection.send(apiResponses.concatObj(apiResponses.JSON.errors.malformedRequest, {"id": msgObject.id}, true));
+              return;
+            }
+          } else {
+            connection.send(apiResponses.strings.errors.missingParameters);
+          }
         } else {
-          connection.sendUTF(apiResponses.strings.errors.malformedRequest);
-          return;
+          connection.send(apiResponses.strings.errors.malformedRequest);
         }
+      } else {
+        connection.send(apiResponses.strings.errors.tooManyRequests);
+        return;
       }
-    } else {
-      connection.sendUTF(apiResponses.strings.errors.tooManyRequests);
-      return;
-    }
+    });
+
+    connection.on('close', () => {
+      logger.log("Connection from " + request.origin + " closed.", 6, false, config.moduleName);
+    });
   });
-});
+}, 2000);
