7fd5bf1e8db82359a9c9a7f7eadd186da0d541f1
Hussain Khalil <hussainpilot05@gmail.com>
2016-09-18 21:57:32 -0400

    Created notify.js file

---
+++ custom_modules/notify.js
@@ -0,0 +1,13 @@
+/*
+Notify Module
+*/
+
+// Require files
+var apiResponses = global.apiResponses,
+logger = global.logger,
+users = require("./users.js");
+
+// Function to send users notifications
+exports.sendNotification = (notification, user) => {
+  
+}
