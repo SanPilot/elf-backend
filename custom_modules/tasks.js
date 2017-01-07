291b6abc37b97789741467ddf908418df5803907
Hussain Khalil <hussainpilot05@gmail.com>
2017-01-07 01:47:36 -0500

    Added tags feature. Fixes #3

--- custom_modules/tasks.js
+++ custom_modules/tasks.js
@@ -42,13 +42,79 @@ var parseTaskBody = (body) => {
   }
 }
 
-var generateTaskBody = (params, connection) => {
-  if(!(params.JWT && params.task && params.task.project && params.task.appliedForPriority !== undefined && params.task.body && params.task.attachedFiles)) {
+// Escape strings to be inserted into regex
+var escRegex = (str) => {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+// Function to search for tags in the db
+var searchTags = (params, connection) => {
+  if(!(params.letters && params.JWT)) {
     connection.send(apiResponses.concatObj(apiResponses.JSON.errors.missingParameters, {"id": params.id}, true));
     return false;
   }
-  var num = 10;
-  if(!(params.task.constructor === {}.constructor && (params.task.project.constructor === num.constructor || params.task.project.constructor === "".constructor) && params.task.appliedForPriority.constructor === true.constructor && params.task.body.constructor === "".constructor && params.task.attachedFiles.constructor === [].constructor)) {
+  if(params.letters.constructor !== String) {
+    connection.send(apiResponses.concatObj(apiResponses.JSON.errors.malformedRequest, {"id": params.id}, true));
+    return false;
+  }
+  if(!users.verifyJWT(params.JWT)) {
+    logger.log("Recieved possibly malacious request with invalid authentication token from " + connection.remoteAddress + ".", 4, true, config.moduleName);
+    connection.send(apiResponses.concatObj(apiResponses.JSON.errors.authFailed, {"id": params.id}, true));
+    return false;
+  }
+  var testExp = "^" + params.letters + ".*$";
+
+  // Search the db for this pattern
+  global.mongoConnect.collection("tags").find({tag:{$regex:testExp}}, (err, docs) => {
+    if(err) {
+      logger.log("Failed database query. (" + err + ")", 2, true, config.moduleName);
+      connection.send(apiResponses.concatObj(apiResponses.JSON.errors.failed, {"id": params.id}, true));
+      return;
+    }
+    connection.send(apiResponses.concatObj(apiResponses.JSON.success, {"id": params.id, "content": docs}, true));
+  });
+}
+
+// This function adds newly defined tags to the database
+var processTag = (name, user, attempt) => {
+  name = name.toLowerCase();
+  attempt = attempt || 0;
+  if(attempt > 5) return; // If it still fails, we can't do anything
+  global.mongoConnect.collection("tags").findOne({tag: name}, (err, doc) => {
+    if(err) {
+      logger.log("Failed database query. (" + err + ")", 2, true, config.moduleName);
+      processTag(name, user, ++attempt);
+      return;
+    }
+    if(doc === null) {
+      // This tag doesn't yet exist in the db, so insert it
+      global.mongoConnect.collection("tags").insertOne({tag: name, user: user, createdAt: Math.floor(new Date() / 1000)}, (err) => {
+        if(err) {
+          logger.log("Failed database query. (" + err + ")", 2, true, config.moduleName);
+          processTag(name, user, ++attempt);
+          return;
+        }
+        logger.log("Added new tag: '"+ name + "'.", 6, false, config.moduleName);
+      });
+    }
+  });
+}
+
+// Function to ensure all elements of array are of the right type
+var checkArray = (array, type) => {
+  for(var i = 0; i < array.length; i++) {
+    if(array[i].constructor !== type) return false;
+  }
+  return true;
+}
+
+// This function is the common code between addTask and modifyTask - it checks all the variables then creates the task's body
+var generateTaskBody = (params, connection) => {
+  if(!(params.JWT && params.task && params.task.project && params.task.appliedForPriority !== undefined && params.task.body && params.task.attachedFiles && params.task.tags)) {
+    connection.send(apiResponses.concatObj(apiResponses.JSON.errors.missingParameters, {"id": params.id}, true));
+    return false;
+  }
+  if(!(params.task.constructor === {}.constructor && params.task.project.constructor === String && params.task.appliedForPriority.constructor === true.constructor && params.task.body.constructor === "".constructor && params.task.attachedFiles.constructor === Array && checkArray(params.task.attachedFiles, String) && params.task.tags.constructor === Array && checkArray(params.task.tags, String))) {
     connection.send(apiResponses.concatObj(apiResponses.JSON.errors.malformedRequest, {id: params.id}, true));
     return false;
   }
@@ -57,6 +123,14 @@ var generateTaskBody = (params, connection) => {
     connection.send(apiResponses.concatObj(apiResponses.JSON.errors.authFailed, {"id": params.id}, true));
     return false;
   }
+
+  // Lowercase all the tags
+  for(var i = 0; i < params.task.tags.length; i++) {
+    params.task.tags[i] = params.task.tags[i].toLowerCase();
+    // Send this to processTag
+    processTag(params.task.tags[i], users.getTokenInfo(params.JWT).payload.user);
+  }
+
   try {
     var parsedBody = parseTaskBody(params.task.body);
     var createdAt = Math.floor(new Date() / 1000);
@@ -73,7 +147,8 @@ var generateTaskBody = (params, connection) => {
       edited: false,
       body: parsedBody.body,
       mentions: parsedBody.mentions,
-      attachedFiles: params.task.attachedFiles
+      attachedFiles: params.task.attachedFiles,
+      tags: params.task.tags
     }
     return {
       parsedBody: parsedBody,
@@ -254,7 +329,8 @@ exports.modifyTask = (params, connection) => {
         edited: true,
         body: parsedBody.body,
         mentions: parsedBody.mentions,
-        attachedFiles: params.task.attachedFiles
+        attachedFiles: params.task.attachedFiles,
+        tags: params.task.tags
       }
 
       // Send notifications to the newly mentioned users
